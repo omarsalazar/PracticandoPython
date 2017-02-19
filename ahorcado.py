1514f302ec99a255b37b99437c36cd197f28a070
omarsalazar <salazarpazomar@gmail.com>
2017-02-19 01:01:28 -0600

    Reintentando el ahorcado y así uwu

--- ahorcado.py
+++ ahorcado.py
@@ -1,11 +1,18 @@
-#Ésta es la palabra que se va a adivinar
-palabra = input("Ingresa la palabra a adivinar uwu")
-#Mientras i sea igual a 1 los turnos van a continuar.
-i= 1
+palabra = input("Ingresa la palabra a adivinar uwu: ")
+
+"""Te da una pista"""
+def pista(palabra):
+    espacios = palabra.count(' ')
+    letras = len(palabra) - espacios
+    print("La palabra tiene {} letras y {} espacios".format(letras, espacios))
+
+pista(palabra)
+
+""" Habrá dos usuarios y dependiendo de el valor 
+ del modulo de turno tirará el usuario 1 o 2."""
+i = 1
 turno = 1
 while (i == 1):
- # Abrá dos usuarios y dependiendo de el valor-
- # -del modulo de turno tirará el usuario 1 o 2.
     if (turno % 2 == 0):
         letra = input("Usuario 2: ")
         eleccion(palabra, letra, opcion)
@@ -13,32 +20,3 @@ while (i == 1):
         letra = input("Usuario 1: ")
         eleccion(palabra, letra, opcion)
 turno = turno + 1
-
-#Esta parte es en donde se elige que quiere poner, letra o numero.
-#Lo que se escribe se compara con la palabra a adivinar-
-#-y te dice si acertaste o fallaste
-def eleccion(palabra, letra, opcion):
-    opcion = input("Deseas ingresar una letra(L) o la palabra(P)")
-    if (opcion == "P" | opcion == "p"):
-        #Llamar a la funcion palabra
-        palabra()
-    elif(opcion == "L" | opcion == "l"):
-        if (palabra == letra):
-            #Llamar a la funcion letra
-            letra()
-    else:
-        print("Ingresa una letra valida")
-
-def palabra():
-	if (palabra == letra):
-        #i se iguala a 0 para terminar el juego.
-		i = 0
-		print("¡Has ganado la partida!")
-	else:
-		print("Fallaste.")
-
-def letra():
-	if (palabra == letra):
-		print("¡Has acertado!")
-	else:
-		print("Fallaste.")
---
+++ panagrama.py
@@ -0,0 +1,13 @@
+def panagrama(s):
+    abecedario = 'zxcvbnmasdfghjklqwertyuiop'
+    status = None
+    for i in range(26):
+        encontrar = s.lower().find(abecedario[i])
+        if encontrar != -1:
+            status = "pangram"
+        else:
+            status = "not pangram"
+            break
+    return status
+s = input('')
+print(panagrama(s))
